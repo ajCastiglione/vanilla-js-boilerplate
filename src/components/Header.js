a1079d7cf546bbe98a4e99f87cdbc395e9e4b1c9
Antonio Castiglione <antonio.castiglione@atlanticbt.com>
2020-04-16 14:42:32 -0400

    init commint, basic file struct

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+const Header = () => {
+    const template = `
+    <header>
+     <h1>Parcel App</h1>
+     <p>Simple boilerplate for projects that require more in-depth JavaScript functionality.</p>    
+    </header>
+    `;
+
+    return template;
+}
+
+export default Header;
